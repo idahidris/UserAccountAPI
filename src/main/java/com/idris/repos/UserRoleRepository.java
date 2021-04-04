0eacb0853b8db91b5263020dafc0cd9d63ca3655
iidah <iidah@nibss-plc.com.ng>
2021-04-04 15:54:51 +0100

    fix code style

--- src/main/java/com/idris/repos/UserRoleRepository.java
+++ src/main/java/com/idris/repos/UserRoleRepository.java
@@ -4,8 +4,6 @@ package com.idris.repos;
 import com.idris.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
 }
