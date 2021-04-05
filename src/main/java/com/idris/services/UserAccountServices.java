2661148e3238f3c8692dba95a32711fcccf7e819
iidah <iidah@nibss-plc.com.ng>
2021-04-05 09:57:54 +0100

    add control for email sending

--- src/main/java/com/idris/services/UserAccountServices.java
+++ src/main/java/com/idris/services/UserAccountServices.java
@@ -43,10 +43,13 @@ public class UserAccountServices {
     private JavaMailSender mailSender;
 
     @Autowired
-     HttpServletRequest httpServletRequest;
+     private HttpServletRequest httpServletRequest;
 
     @Value("${spring.mail.username}")
     private String fromAddress;
+
+    @Value("${application.enable.mail}")
+    private boolean enableMail;
 
 
     public List<UserAccount> findAll(int page, int pageSize){
@@ -232,30 +235,32 @@ public class UserAccountServices {
 
     private void sendVerificationByEmail(UserAccount user, String siteURL)
             throws MessagingException, UnsupportedEncodingException {
-        String toAddress = user.getEmail();
-        String senderName = "UserAccountAPI";
-        String subject = "Please verify your registration";
-        String content = "Dear [[name]],<br>"
-                + "Please click the link below to activate your registration:<br>"
-                + "<h3><a href=\"[[URL]]\" target=\"_self\">ACTIVATE</a></h3>"
-                + "Thank you,<br>"
-                + "UserAccountAPI.";
+        if(enableMail) {
+            String toAddress = user.getEmail();
+            String senderName = "UserAccountAPI";
+            String subject = "Please verify your registration";
+            String content = "Dear [[name]],<br>"
+                    + "Please click the link below to activate your registration:<br>"
+                    + "<h3><a href=\"[[URL]]\" target=\"_self\">ACTIVATE</a></h3>"
+                    + "Thank you,<br>"
+                    + "UserAccountAPI.";
 
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
 
-        helper.setFrom(fromAddress, senderName);
-        helper.setTo(toAddress);
-        helper.setSubject(subject);
+            helper.setFrom(fromAddress, senderName);
+            helper.setTo(toAddress);
+            helper.setSubject(subject);
 
-        content = content.replace("[[name]]", user.getFirstname());
-        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
+            content = content.replace("[[name]]", user.getFirstname());
+            String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
 
-        content = content.replace("[[URL]]", verifyURL);
+            content = content.replace("[[URL]]", verifyURL);
 
-        helper.setText(content, true);
+            helper.setText(content, true);
 
-        mailSender.send(message);
+            mailSender.send(message);
+        }
 
     }
 
